859c69e2f89981234ff397e78b2ad21a03b08fc3
akdeas oktanae <akdeaso@gmail.com>
2022-12-06 22:27:16 +0700

    add login and register page

--- src/main.jsx
+++ src/main.jsx
@@ -1,27 +1,33 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Route,
-} from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
 
 //import pages ny dibawahsini
-import TesDaze from './pages/tes';
+import TesDaze from "./pages/tes";
+import LoginSide from "./pages/LoginSide";
+import RegisterSide from "./pages/RegisterSide";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <TesDaze/>,
+    element: <TesDaze />,
   },
   {
     path: "/sini",
     element: <>mengokil</>,
   },
+  {
+    path: "/login",
+    element: <LoginSide />,
+  },
+  {
+    path: "/register",
+    element: <RegisterSide />,
+  },
 ]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>
-)
+);
